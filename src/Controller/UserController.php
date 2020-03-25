e50acd56d5aaa7cfa7f6340d633efe2e9bb0514e
sorayaleon <soraya.l.g.88@gmail.com>
2020-03-25 12:54:42 +0100

    login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
@@ -31,12 +32,23 @@ class UserController extends AbstractController
             $em->persist($user);
             $em->flush();
             
-            return $this->redirectToRoute('index');
+            return $this->redirectToRoute('tareas');
             
         }
                 
         return $this->render('user/registro.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+    
+    public function login(AuthenticationUtils $authenticationUtils){
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        $lastUserName = $authenticationUtils->getLastUsername();
+        
+        return $this->render('user/login.html.twig', array(
+            'error' => $error,
+            'last_username' => $lastUserName
+        ));
     }
 }
